dfa33fbfb98d2b01a03a30cdc26b00a7cbacb28c
MaximeF <fumelmax@gmail.com>
2019-04-04 10:40:17 +0200

    queue helper

--- src/main/java/fr/ensibs/joram/Helper.java
+++ src/main/java/fr/ensibs/joram/Helper.java
@@ -2,6 +2,7 @@ package fr.ensibs.joram;
 
 
 import javax.jms.JMSException;
+import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.Topic;
 import javax.naming.Context;
@@ -9,7 +10,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public class Helper {
-
+	
     public static Topic getTopic(Session sess, String topic) {
         Context context = null;
         try {
@@ -31,5 +32,28 @@ public class Helper {
             e.printStackTrace();
         }
         return t;
+    }
+    
+    public static Queue getQueue(Session sess, String queue) {
+        Context context = null;
+        try {
+            context = new InitialContext();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        Queue q = null;
+        try {
+            q = (Queue) context.lookup(queue);
+        } catch (NamingException e) {}
+
+        try {
+            if (q == null) {
+                q = sess.createQueue(queue);
+                context.bind(queue, q);
+            }
+        } catch (JMSException | NamingException e) {
+            e.printStackTrace();
+        }
+        return q;
     }
 }
